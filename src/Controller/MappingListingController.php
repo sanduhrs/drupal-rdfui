ca719e98ff3737e8e3d0a5ec2630d4f8818bf087
Sachini Aparna Herath <sachini.mc@gmail.com>
2014-06-12 13:19:09 +0530

    fixed errors in rdfui module

--- src/Controller/MappingListingController.php
+++ src/Controller/MappingListingController.php
@@ -10,12 +10,19 @@ class MappingListingController implements  ContainerInjectionInterface{
 
     public static function create(ContainerInterface $container){
         return new static($container->get('module _handler'));
-
     }
 
+    /**
+     * Return the 'Mapping Listing PAge' page.
+     *
+     * @return string
+     *   A render array containing our page content.
+     */
     public function mappingListingPage(){
-        return array('$markup'=>t('Hello! This is rdfui module.'));
+        return array(
+            '#markup'=>t('Hello! This is rdfui module.')
+        );
+
     }
 }
 
-?>
