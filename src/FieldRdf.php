6fd6683d8d5ceba7aca33be05ce5bd9bac122ed4
Sachini Aparna Herath <sachini.mc@gmail.com>
2014-06-25 18:16:42 +0530

    Added new tab and sample form

---
+++ src/FieldRdf.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Definition of Drupal\rdfui\Form\FieldRdf.
+ */
+
+namespace Drupal\rdfui\Form;
+
+
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Component\Utility\String;
+
+/**
+ * Form controller for the node edit forms.
+ */
+class NodeForm extends ContentEntityForm {
+
+}
