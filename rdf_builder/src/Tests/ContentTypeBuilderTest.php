e4bb2b09fb0c90872eed014cafb4a72ab1d05b41
Sachini Aparna Herath <sachini.mc@gmail.com>
2014-08-01 14:24:06 +0530

    Updated Content Type Builder and created test files

---
+++ rdf_builder/src/Tests/ContentTypeBuilderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rdf_builder\Tests\ContentTypeBuilderTest.
+ */
+
+namespace Drupal\rdf_builder\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Provides common functionality for the Field UI test classes.
+ */
+abstract class ContentTypeBuilderTest extends WebTestBase {
+}
